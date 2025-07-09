aa82a3c1abb8397a1d1bb0da25e6c36dfb45775c
Samara Flamini Kiihl <samara.kiihl@gmail.com>
2025-07-09 14:33:50 -0300

    Update functions_annex.R

--- R/functions_annex.R
+++ R/functions_annex.R
@@ -270,7 +270,7 @@ sim_multi=function(n=1000, NM=3,
 }
 
 order.data = function(Exposure,Outcome,Mediators,P){
-  J=dim(Mediators)[1]#length(Exposure)
+  J=dim(Mediators)[1]
   K=dim(Mediators)[2]
 
   z=matrix(0,(K+1)*J,1)
@@ -278,7 +278,6 @@ order.data = function(Exposure,Outcome,Mediators,P){
   i=1
   p=1
   for(k in 1:K){
-    #print(paste("Pour la colonne",k,"on remplit de",i,"à",(i+J-1),sep=' '))
     z[i:(i+J-1),1]=Mediators[,k]
     X[i:(i+J-1),p:(p+P-1)]=as.matrix(Exposure[1:J,1:P])
     i=i+J
@@ -290,31 +289,7 @@ order.data = function(Exposure,Outcome,Mediators,P){
   X[seq((K+1)*J-J+1,(K+1)*J),((P+1)*K+1):((P+1)*K+P)]=as.matrix(Exposure)
 
   group=c(rep(seq(1,K),each=P),seq(1,K),K+1)
-  X=X[,order(group)] # on reordonne X pour gglasso
+  X=X[,order(group)]
   group=c(rep(seq(1,K),each=P+1),K+1)
   return(list(z,X,group))
 }
-
-# order.data=function(Exposure,Outcome,Mediators){
-#   J=length(Exposure)
-#   K=dim(Mediators)[2]
-#
-#   z=matrix(0,(K+1)*J,1)
-#   X=matrix(0,(K+1)*J,2*K+1)
-#   i=1
-#   for(k in 1:K){
-#     #print(paste("Pour la colonne",k,"on remplit de",i,"à",(i+J-1),sep=' '))
-#     z[i:(i+J-1),1]=Mediators[,k]
-#     X[i:(i+J-1),k]=Exposure
-#     i=i+J
-#   }
-#
-#   z[i:(i+J-1),1]=Outcome
-#   X[seq((K+1)*J-J+1,(K+1)*J),seq((K+1),K+K)]=as.matrix(Mediators)
-#   X[seq((K+1)*J-J+1,(K+1)*J),2*K+1]=Exposure
-#
-#   group=c(rep(seq(1,K),2),K+1)
-#   X=X[,order(group)] # on reordonne X pour gglasso
-#   group=c(rep(seq(1,K),each=2),K+1)
-#   return(list(z,X,group))
-# }
